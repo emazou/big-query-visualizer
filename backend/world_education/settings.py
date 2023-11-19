74676d3e231a8446e7179d34d64299c99ad4c28e
Estefania <emazou@geosat.com.co>
2023-11-18 20:19:48 -0500

    Docker containers

--- backend/world_education/settings.py
+++ backend/world_education/settings.py
@@ -88,10 +88,18 @@ WSGI_APPLICATION = 'world_education.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',                      
+        'USER': 'postgres',                    
+        'PASSWORD': 'secretpassword',      
+        'HOST': 'db',                
+        'PORT': '5432',
     }
 }
+#'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
 
 
 # Password validation
